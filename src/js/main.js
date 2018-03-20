d361dffab440ee500439675457a800c33c41ca9c
Emma O'Neill <emmaruthoneill@gmail.com>
2018-03-20 15:45:16 -0700

    cleaning up navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 require("./lib/social"); //Do not delete
 
+// smooth scrolling down to nav elements
 $(document).on('click', 'a[href^="#"]', function(e) {
     // target element id
     var id = $(this).attr('href');
@@ -16,70 +17,57 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     $('body, html').animate({scrollTop: pos});
 });
 
+var navID = document.getElementById("nav");
+var profile_idx = -1;
+var a,b,c,d;
+var navoffset = 70;
+
+// find positions for the different section heds
+window.onload = function () {
+  a = $("#state").position().top - navoffset;
+  b = $("#region").position().top - navoffset;
+  c = $("#sanfrancisco").position().top - navoffset;
+  d = $("#judiciary").position().top - navoffset;
+  scroll = [a,b,c,d];
+}
+
+// scroll function
+var y, section_idx;
 function activate() {
   var sticker = document.getElementById('stick-me');
   var sticker_ph = document.getElementById('stick-ph');
-  var window_top = document.body.scrollTop;
+  var window_top = document.documentElement.scrollTop || document.body.scrollTop;//document.body.scrollTop;
   var div_top = document.getElementById('stick-here').getBoundingClientRect().top + window_top - 37;
-  // var long = document.getElementById('long');
+  var y = $(this).scrollTop();
 
-  if (window_top > div_top) {
+  // unfix secondary nav and remove nav highlights
+  if (window_top <= div_top){
+    for (var i=0; i<scroll.length; i++) {
+      document.getElementById("nav-element"+i).classList.remove('activelink');
+    }
+    sticker.classList.remove('fixed-second');
+    sticker_ph.style.display = 'none'; // removes placeholder
+
+  // fix secondary nav and highlight section in nav
+  } else if (window_top > div_top) {
       sticker.classList.add('fixed-second');
       sticker_ph.style.display = 'block'; // puts in a placeholder for where sticky used to be for smooth scrolling
-      // long.style.display = 'inline-block';
-  } else {
-      sticker.classList.remove('fixed-second');
-      sticker_ph.style.display = 'none'; // removes placeholder
-      // long.style.display = 'none';
+
+      // hide all active links if reader is at top of page
+      if (window_top >= a) {
+        // find out which section they are in
+        for (var i = 0; i < scroll.length; i++) {
+          if (y > scroll[i]) {
+            section_idx = i;
+          }
+          document.getElementById("nav-element"+i).classList.remove('activelink');
+        }
+        // set section to be highlighted in the nav
+        if (scroll[section_idx]){
+          document.getElementById("nav-element"+section_idx).classList.add('activelink');
+        }
+      }
   }
 }
 
 window.onscroll = function() {activate()};
-
-
-
-// var navID = document.getElementById("nav");
-// var navposition = 400;//document.getElementById("link-nav").offsetTop+40;
-// var profile_idx = -1;
-// var a,b,c,d,e,f,concdiv;
-// window.onload = function () {
-//   var window_top = document.body.scrollTop-30;
-//   a = document.getElementById('profileiain').getBoundingClientRect().top + window_top;
-//   b = document.getElementById('profilelauren').getBoundingClientRect().top + window_top;
-//   c = document.getElementById('profilehilary').getBoundingClientRect().top + window_top;
-//   d = document.getElementById('profilejorge').getBoundingClientRect().top + window_top;
-//   e = document.getElementById('profilebalint').getBoundingClientRect().top + window_top;
-//   f = document.getElementById('profilegreg').getBoundingClientRect().top + window_top;
-//   scroll = [a,b,c,d,e,f];
-//   concdiv= document.getElementById('conclusion').getBoundingClientRect().top + window_top;
-// }
-//
-// var y;
-// var navDisplay = function() {
-//   // var y = window.scrollY;
-//   y = $(window).scrollTop();
-//   if (y >= navposition) {
-//     navID.className = "fixed show";
-//   } else {
-//     navID.className = "fixed hide";
-//   }
-//   if (y > concdiv || y < a) {
-//     for (var i=0; i<6; i++) {
-//       document.getElementById("prof-link-"+shortkeyList[i]).classList.remove('activelink');
-//     }
-//   } else {
-//     for (var i = 0; i < 6; i++) {
-//       if (y > scroll[i]) {
-//         profile_idx = i;
-//       }
-//     }
-//     if (shortkeyList[profile_idx]){
-//       for (var i=0; i<6; i++) {
-//         document.getElementById("prof-link-"+shortkeyList[i]).classList.remove('activelink');
-//       }
-//       document.getElementById("prof-link-"+shortkeyList[profile_idx]).classList.add('activelink');
-//     }
-//   }
-// };
-// window.addEventListener("scroll", navDisplay);
-//
