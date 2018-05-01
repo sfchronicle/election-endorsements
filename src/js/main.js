534c7d5370744ae62c8b35bc589af5dd5d87223a
Emma O'Neill <emmaruthoneill@gmail.com>
2018-05-01 16:23:50 -0700

    this does what i want with the video and podcast buttons but i would prefer it to scroll smoothly

--- src/js/main.js
+++ src/js/main.js
@@ -57,14 +57,14 @@ for (var t = 0; t < seemoreButtons.length; t++){
           $("#extras-"+buttonidx).removeClass("show");
           $('body').removeClass('noscroll');
           if (screen.width <= 480){
-            $(this).find('.read-more-less').html("Read more");
+            $(this).find('.read-more-less').html("Learn more");
             $(this).find('i').toggleClass('fa-caret-down fa-caret-up');
           }
         } else {
           $("#extras-"+buttonidx).addClass("show");
           $('body').addClass('noscroll');
           if (screen.width <= 480){
-            $(this).find('.read-more-less').html("Read less");
+            $(this).find('.read-more-less').html("See less");
             $(this).find('i').toggleClass('fa-caret-down fa-caret-up');
           }
         }
@@ -72,34 +72,6 @@ for (var t = 0; t < seemoreButtons.length; t++){
     })(td);
   };
 }
-
-// // buttons to open expanded sections
-// var seemoreButtons = document.getElementsByClassName("see-more-mobile");
-// console.log(seemoreButtons);
-// for (var t = 0; t < seemoreButtons.length; t++){
-//   console.log(t);
-//   console.log(seemoreButtons[t])
-//   var td = document.getElementById(seemoreButtons[t].id);
-//   td.addEventListener('click', function(){
-//     console.log(td.id.split("read-more-")[1])
-//     var buttonidx = td.id.split("read-more-")[1];
-//     if (document.getElementById("extras-"+buttonidx).classList.contains("show")){
-//       $("#extras-"+buttonidx).removeClass("show");
-//       $('body').removeClass('noscroll');
-//       if (screen.width <= 480){
-//         $(this).find('.read-more-less').html("Read more");
-//         $(this).find('i').toggleClass('fa-caret-down fa-caret-up');
-//       }
-//     } else {
-//       $("#extras-"+buttonidx).addClass("show");
-//       $('body').addClass('noscroll');
-//       if (screen.width <= 480){
-//         $(this).find('.read-more-less').html("Read less");
-//         $(this).find('i').toggleClass('fa-caret-down fa-caret-up');
-//       }
-//     }
-//   });
-// }
 
 // buttons to close expanded sections on desktop
 if (screen.width >= 480) {
@@ -113,7 +85,7 @@ if (screen.width >= 480) {
           var buttonidx = _td.id.split("exit-")[1];
           $("#extras-"+buttonidx).removeClass("show");
           $('body').removeClass('noscroll');
-          $(this).find('.read-more-less').html("Read more");
+          $(this).find('.read-more-less').html("Learn more");
           // $(this).find('i').toggleClass('fa-caret-down fa-caret-up');
         });
       })(td);
@@ -121,3 +93,22 @@ if (screen.width >= 480) {
   }
 
 }
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     console.log("did this even work");
+//     e.preventDefault();
+//
+//     document.querySelector(this.getAttribute('href')).scrollIntoView({
+//         behavior: 'smooth'
+//     });
+//   });
+// });
+// 
+// $(document).on('click', 'a[href^="#"]', function (event) {
+//     event.preventDefault();
+//
+//     $('.extras-overlay').animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 500);
+// });
